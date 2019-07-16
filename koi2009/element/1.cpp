6efcd4b09079fc2c6a78ba26157ce84004a3139f
Sungguk Cha <31852410+sunggukcha@users.noreply.github.com>
2019-07-16 23:44:05 +0900

    Create 1.cpp

---
+++ koi2009/element/1.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int arr[5000001];
+    long long sum = 0;
+    scanf("%d", &n);
+    for(int i=0;i<n;i++){
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    sort(arr, arr+n);
+    if(n % 2 == 0)
+        printf("%d ", arr[n/2-1]);
+    else
+        printf("%d ", arr[n/2]);
+    double ans = (double)sum / n;
+    
+    return 0;
+}
